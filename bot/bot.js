3d2c422139dd9a00e0a012df46598ae413c0f042
Kirill Oreshkevich <oreshaodnoglazy@gmail.com>
2025-08-10 23:47:27 +0300

    feat: add new comand

--- bot/bot.js
+++ bot/bot.js
@@ -4,6 +4,7 @@ const bot = new TelegramBot(token, { polling: true });
 
 require('./comands/quiz')(bot);
 require('./comands/quizIrr')(bot);
+require('./comands/comands')(bot);
 require('./handlers/callbackHandler')(bot);
 require('./handlers/messageHandler')(bot);
 
